e4a1bb626d1e8f3ad647f338f44691664df82f88
salviega <salviega6@gmail.com>
2026-01-30 20:17:30 -0500

    ✨ Added PaymentManager contract and compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,18 @@ import '@nomicfoundation/hardhat-toolbox-viem'
 import type { HardhatUserConfig } from 'hardhat/config'
 
 const config: HardhatUserConfig = {
-	solidity: '0.8.28'
+	solidity: {
+		compilers: [
+			{
+				version: '0.8.30',
+				settings: {
+					optimizer: { enabled: true, runs: 200 },
+					evmVersion: 'osaka',
+					viaIR: false
+				}
+			}
+		]
+	}
 }
 
 export default config
